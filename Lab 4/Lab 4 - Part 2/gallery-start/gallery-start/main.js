7aaefca38d997c7f55c8d419ca68e82ba2d77158
Ameilia <ameiliamerhair@gmail.com>
2025-04-03 18:56:51 -0400

    Commit 3
    Halfway through.

--- Lab 4/Lab 4 - Part 2/gallery-start/gallery-start/main.js
+++ Lab 4/Lab 4 - Part 2/gallery-start/gallery-start/main.js
@@ -5,10 +5,27 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-
+const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
+const alts = {
 /* Declaring the alternative text for each image file */
+  'pic1.jpg' : 'Closeup of a human eye',
+  'pic2.jpg' : 'Rock that looks like a wave',
+  'pic3.jpg' : 'Purple and white pansies',
+  'pic4.jpg' : 'Section of wall from a pharoah\'s tomb',
+  'pic5.jpg' : 'Large moth on a leaf'
+}
 
 /* Looping through images */
+for (const image of images) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${image}`);
+    newImage.setAttribute('alt', alts[image]);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', e => {
+      displayedImage.src = e.target.src;
+      displayedImage.alt = e.target.alt;
+    });
+  }
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
