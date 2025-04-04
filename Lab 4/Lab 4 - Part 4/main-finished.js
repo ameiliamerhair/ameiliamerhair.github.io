36d0c0ba29b74c6359cfa256cc2de249fc6b1a94
Ameilia <ameiliamerhair@gmail.com>
2025-04-03 20:49:03 -0400

    Commit 5
    Testing to see if fully functional.

--- Lab 4/Lab 4 - Part 4/main-finished.js
+++ Lab 4/Lab 4 - Part 4/main-finished.js
@@ -6,6 +6,9 @@ const ctx = canvas.getContext("2d");
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
+const ballCounterElement = document.querySelector('.ballcounter');
+let ballCounter = 0;
+
 // function to generate random number
 
 function random(min, max) {
@@ -78,6 +81,7 @@ class Ball extends Shape {
     }
   }
 }
+
 class EvilCircle extends Shape{
     constructor(x, y){
         super(x, y, 20, 20);
@@ -102,8 +106,46 @@ class EvilCircle extends Shape{
             }
           });
     }
+    draw() {
+        ctx.beginPath();
+        ctx.strokeStyle = this.color;
+        ctx.lineWidth = 3;
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.stroke();
+    }
+    checkBounds() {
+        if ((this.x + this.size) >= width) {
+          this.x -= this.size;
+        }
+    
+        if ((this.x - this.size) <= 0) {
+          this.x += this.size;
+        }
+    
+        if ((this.y + this.size) >= height) {
+          this.y -= this.size;
+        }
+    
+        if ((this.y - this.size) <= 0) {
+          this.y += this.size;
+        }
+    }
+    collisionDetect() {
+        for (const ball of balls) {
+          if (ball.exists) {
+            const dx = this.x - ball.x;
+            const dy = this.y - ball.y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+    
+            if (distance < this.size + ball.size) {
+              ball.exists = false;
+              count--;
+              para.textContent = 'Ball count: ' + count;
+            }
+          }
+        }
+      }
 }
-
 
 const balls = [];
 
@@ -121,18 +163,27 @@ while (balls.length < 25) {
   );
 
   balls.push(ball);
+  count++;
+  para.textContent = 'Ball count: ' + count;
 }
+
+const evilBall = new EvilCircle(random(0, width), random(0, height));
 
 function loop() {
   ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
   ctx.fillRect(0, 0, width, height);
 
   for (const ball of balls) {
-    ball.draw();
-    ball.update();
-    ball.collisionDetect();
+    if (ball.exists){
+        ball.draw();
+        ball.update();
+        ball.collisionDetect();
+    }
   }
 
+  evilBall.draw();
+  evilBall.checkBounds();
+  evilBall.collisionDetect();
   requestAnimationFrame(loop);
 }
 
