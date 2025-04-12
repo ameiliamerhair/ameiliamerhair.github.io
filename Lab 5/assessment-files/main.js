7c044ade9827198f2b997386a7973d3cb0473991
Ameilia <ameiliamerhair@gmail.com>
2025-04-11 22:08:00 -0400

    Final Commit
    Final commit

--- Lab 5/assessment-files/main.js
+++ Lab 5/assessment-files/main.js
@@ -45,3 +45,67 @@ function submitComment() {
   nameField.value = '';
   commentField.value = '';
 }
+
+// functionality for audio controls
+const audioPlayer = document.querySelector("audio");
+const playPauseBtn = document.querySelector(".playpause");
+const stopBtn = document.querySelector(".stop");
+const transcriptBtn = document.querySelector(".transcript");
+const timeLabel = document.querySelector(".time");
+const transcript = document.querySelector("#transcript");
+
+audioPlayer.removeAttribute("controls");
+
+transcriptBtn.addEventListener("click", function () {
+  if (transcript.classList.contains("transcript-hidden")) {
+    transcript.classList.remove("transcript-hidden");
+    transcript.classList.add("transcript-active");
+  } else {
+    transcript.classList.remove("transcript-active");
+    transcript.classList.add("transcript-hidden");
+  }
+});
+
+playPauseBtn.addEventListener("click", function () {
+  if (audioPlayer.paused) {
+    audioPlayer.play();
+    playPauseBtn.textContent = "Pause";
+  } else {
+    audioPlayer.pause();
+    playPauseBtn.textContent = "Play";
+  }
+});
+
+stopBtn.addEventListener("click", function () {
+  audioPlayer.pause();
+  audioPlayer.currentTime = 0;
+  playPauseBtn.textContent = "Play";
+});
+
+audioPlayer.addEventListener("timeupdate", function () {
+  let minutes = Math.floor(audioPlayer.currentTime / 60);
+  let seconds = Math.floor(audioPlayer.currentTime - minutes * 60);
+  let minuteValue;
+  let secondValue;
+  let mediaTime;
+
+  if (minutes < 10) {
+    minuteValue = "0" + minutes;
+  } else {
+    minuteValue = minutes;
+  }
+
+  if (seconds < 10) {
+    secondValue = "0" + seconds;
+  } else {
+    secondValue = seconds;
+  }
+
+  mediaTime = minuteValue + ":" + secondValue;
+  timeLabel.textContent = mediaTime;
+});
+
+audioPlayer.addEventListener("ended", function() {
+  audioPlayer.currentTime = 0;
+  playPauseBtn.textContent = "Play";
+});
